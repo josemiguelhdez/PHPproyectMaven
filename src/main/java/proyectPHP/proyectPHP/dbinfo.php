85421e40a7bd923d5754dc5841a58f3b1938a0be
Jose <devildeath1991@gmail.com>
2014-01-06 22:03:58 +0000

    subido con maven

---
+++ src/main/java/proyectPHP/proyectPHP/dbinfo.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * @brief Datos de la base de datos local que usamos
+ * @param Username Referencia al nombre de usuario de la BD
+ * @param Password Referencia a la contraseña del usuario antes citado de la BD
+ * @param Database Referencia a la propia BD
+ */
+
+$username="a6708847_TR";
+$password="pedro390";
+$database="a6708847_TR";
+?>
